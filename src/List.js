b1683e63b5747716b0d0475bb69416341e28f131
Alejandro Acuaviva <alejandro.acuavivaplazuelo@everis.nttdata.com>
2023-05-02 14:53:08 +0200

    practica memo

--- src/List.js
+++ src/List.js
@@ -2,20 +2,29 @@ import { useEffect, useState } from 'react';
 
 function List() {
   const [animals, setAnimals] = useState([]);
+  const [searcher, setSearcher] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/animals?name=dog', {
+  const handleChange = evt => {
+    setSearcher(evt.target.value);
+  }
+
+  const onClickFilter = () => {
+    fetch(`https://api.api-ninjas.com/v1/animals?name=${searcher}`, {
       headers: {
         'X-Api-Key': 'xOgcBLTk9ZvG5bGTzeP6nw==0rPJoVOZEFUD0zmp'
       }
     })
       .then(response => response.json())
       .then(data => setAnimals(data));
-  }, [])
+  }
   
   return (
     <div>
       <h1>Lista de animales</h1>
+      <form>
+        <input type='text' value={searcher} onChange={handleChange}/>
+      </form>
+      <button onClick={onClickFilter}>Filtrar</button>
       <ul>
         {animals.map(a => {
           return <li>{a.name}</li>
