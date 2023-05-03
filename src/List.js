a98b2a6385f77412e18e01a06a7d86b5cf3e37ef
Alejandro Acuaviva <alejandro.acuavivaplazuelo@everis.nttdata.com>
2023-05-03 07:55:31 +0200

    mejoras practica

--- src/List.js
+++ src/List.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 
 function List() {
   const [animals, setAnimals] = useState([]);
-  const [searcher, setSearcher] = useState([]);
+  const [searcher, setSearcher] = useState('');
 
   const handleChange = evt => {
     setSearcher(evt.target.value);
   }
 
-  const onClickFilter = () => {
+  const handleSubmit = () => {
     fetch(`https://api.api-ninjas.com/v1/animals?name=${searcher}`, {
       headers: {
         'X-Api-Key': 'xOgcBLTk9ZvG5bGTzeP6nw==0rPJoVOZEFUD0zmp'
@@ -21,10 +21,10 @@ function List() {
   return (
     <div>
       <h1>Lista de animales</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type='text' value={searcher} onChange={handleChange}/>
+        <button>Buscar</button>
       </form>
-      <button onClick={onClickFilter}>Filtrar</button>
       <ul>
         {animals.map(a => {
           return <li>{a.name}</li>
